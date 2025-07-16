fa3346588e224084cb48b2e4d1c034f4ac77c44b
Swapnil Garg <swapnil.garg@gep.com>
2025-07-16 16:14:27 +0530

    prod add mongodb

--- JirAutomate/Services/UserService.cs
+++ JirAutomate/Services/UserService.cs
@@ -5,11 +5,18 @@ namespace JirAutomate.Services;
 
 public class UserService
 {
+    
     private readonly IMongoCollection<User> _users;
+    private readonly string _connectionString;
 
     public UserService(IConfiguration config)
     {
+        DotNetEnv.Env.Load();
+        _connectionString = Environment.GetEnvironmentVariable("MONGODB_URL") ?? throw new Exception("Missing MONGODB_URL");
+        //var client = new MongoClient(_connectionString); //this is for prod
         var client = new MongoClient(config["MongoDBSettings:ConnectionString"]);
+        Console.WriteLine("Connecting to MongoDB: " + _connectionString);
+        client.ListDatabaseNames();
         var db = client.GetDatabase(config["MongoDBSettings:DatabaseName"]);
         _users = db.GetCollection<User>(config["MongoDBSettings:CollectionName"]);
     }
